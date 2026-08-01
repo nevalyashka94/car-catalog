838b3a615d407c5553450cf505bef5f579fbd8e9
nevalyashka94 <tcvnw54jd6@privaterelay.appleid.com>
2026-08-01 10:50:59 +0300

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+export default {
+  darkMode: "class",
+
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}"
+  ],
+
+  theme: {
+    extend: {
+      colors: {
+        primary: "#2563eb",
+        secondary: "#0f172a"
+      }
+    }
+  },
+
+  plugins: []
+};
